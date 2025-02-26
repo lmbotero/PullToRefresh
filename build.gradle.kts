52cd764d89d5f22bb9939222c0c06468c8182283
Luis Miguel Botero Tobon <luismiguel.botero@globant.com>
2025-02-26 09:37:18 -0500

    Refactor: Removes ktlint plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,15 +4,9 @@ plugins {
     alias(libs.plugins.compose).apply(false)
     alias(libs.plugins.android.application).apply(false)
     alias(libs.plugins.android.library).apply(false)
-    alias(libs.plugins.jlleitschuh.ktlint)
     alias(libs.plugins.dokka)
 }
 
 subprojects {
-    apply(plugin = "org.jlleitschuh.gradle.ktlint")
     apply(plugin = "org.jetbrains.dokka")
-
-    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
-        version.set("1.5.0")
-    }
 }
