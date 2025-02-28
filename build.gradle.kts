2df50818c7ebe6532a542c4b9479cf7f5f233a5a
Luis Miguel Botero Tobon <lmiguel1995@gmail.com>
2025-02-28 16:17:27 -0500

    Refactor: Updates Gradle configuration and adds Ktlint plugin.
    - Enables ignoring disabled targets in Kotlin Native.
    - Disables warnings for experimental Gradle plugin mode in Dokka.
    - Adds the Ktlint plugin to subprojects.
    - Configures Ktlint with version 1.5.0 and enables experimental rules.
    - Disables Ktlint for generated files.
    - Updates the version catalog to include the jlleitschuh Ktlint plugin.

--- build.gradle.kts
+++ build.gradle.kts
@@ -5,8 +5,15 @@ plugins {
     alias(libs.plugins.android.application).apply(false)
     alias(libs.plugins.android.library).apply(false)
     alias(libs.plugins.dokka)
+    alias(libs.plugins.jlleitschuh.ktlint)
 }
 
 subprojects {
     apply(plugin = "org.jetbrains.dokka")
+    apply(plugin = "org.jlleitschuh.gradle.ktlint")
+
+    configure<org.jlleitschuh.gradle.ktlint.KtlintExtension> {
+        version.set("1.5.0")
+        enableExperimentalRules.set(true)
+    }
 }
